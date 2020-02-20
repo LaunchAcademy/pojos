a611f6cf830f9b6f31396986abc3a98db702b552
Daniel <hanakoscar@outlook.com>
2020-02-20 15:03:35 -0500

    the actual best for realz yo

---
+++ src/Startrek.js
@@ -0,0 +1,12 @@
+class StarTrek {
+  constructor(series, ship, captain) {
+    this.series = series
+    this.ship = ship
+    this.captain = captain
+  }
+
+  theActualBest() {
+    best = "The best series is Voyager, the best ship is the Defiant, the best captain is Kirk!"
+    return best
+  }
+}
